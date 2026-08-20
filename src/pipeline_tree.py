49585888b9766522e503bf0675abbdf11e7d7024
YugamdeepGoyal <yugamdeep@gmail.com>
2026-08-20 15:59:29 +0530

    Adding pipeline for tree models and adding random forest model code

---
+++ src/pipeline_tree.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+def get_preprocessor():
+    cat_cols = ["Gender", "Senior Citizen", "Partner", "Dependents", "Phone Service", "Multiple Lines", "Internet Service", "Online Security", "Online Backup", "Device Protection", "Tech Support", "Streaming TV", "Streaming Movies", "Contract", "Paperless Billing", "Payment Method"]
+    pass_through = ["Tenure Months", "Monthly Charges", "Total Charges"]
+
+
+    encoder = Pipeline([
+        ("encoder", OneHotEncoder(drop="first", handle_unknown="ignore"))
+    ])
+
+    preprocessor = ColumnTransformer([
+        ("encoder_col", encoder, cat_cols),
+        ("passthrough", "passthrough", pass_through)
+    ])
+
+    return preprocessor
