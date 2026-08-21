e23d22068065bb591f34c7374274cb50c5154b67
YugamdeepGoyal <yugamdeep@gmail.com>
2026-08-21 21:10:18 +0530

    MODIFIED FILES

--- src/metrics.py
+++ src/metrics.py
@@ -8,16 +8,19 @@ from sklearn.metrics import (
     recall_score,
     precision_score,
     roc_auc_score,
+    roc_curve
 )
 
 
-def metrics(y_true, y_pred, y_proba):
+def metrics(y_true, y_pred, y_proba, model_name=None):
     print(f"Accuracy score: {accuracy_score(y_true, y_pred)}")
     print(f"recall score: {recall_score(y_true, y_pred)}")
     print(f"precision score: {precision_score(y_true, y_pred)}")
     print(f"f1 score: {f1_score(y_true, y_pred)}")
     print(f"ROC-AUC score: {roc_auc_score(y_true, y_proba)}")
+
     print(f"Classification Report\n{classification_report(y_true, y_pred)}")
+
     cm = confusion_matrix(y_true, y_pred)
     plt.title("Confusion Matrix")
     sns.heatmap(
@@ -32,4 +35,15 @@ def metrics(y_true, y_pred, y_proba):
     )
     plt.xlabel("Predicted")
     plt.ylabel("True")
+    plt.show()
+
+    fpr, tpr, thresholds = roc_curve(y_true, y_proba)
+    plt.plot(fpr, tpr)
+    plt.plot([0, 1], [0, 1], color="navy", lw=2, linestyle="--", label="ROC CURVE")
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC AUC Curve")
+    plt.legend(loc="lower right")
     plt.show()
